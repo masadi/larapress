e0747c9f67c45d86538eb417e79cc00a071dbc4b
masadi <masadi.com@gmail.com>
2022-08-29 02:44:01 +0700

    Update home.blade.php

--- resources/views/content/home.blade.php
+++ resources/views/content/home.blade.php
@@ -6,8 +6,8 @@
   <div class="container">
     <div class="row">
       <div class="col-lg-6 d-flex flex-column justify-content-center">
-        <h1 data-aos="fade-up">Kami hadir untuk menjadi solusi pendidikan modern yang agamis</h1>
-        <h2 data-aos="fade-up" data-aos-delay="400">Dengan didukung oleh stackholders yang mumpuni dibidangnya</h2>
+        <h1 data-aos="fade-up">We offer modern solutions for growing your business</h1>
+        <h2 data-aos="fade-up" data-aos-delay="400">We are team of talented designers making websites with Bootstrap</h2>
       </div>
       <div class="col-lg-6 hero-img" data-aos="zoom-out" data-aos-delay="200">
         <img src="assets/img/hero-img.png" class="img-fluid" alt="">
